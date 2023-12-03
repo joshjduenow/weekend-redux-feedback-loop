98c06ef49eecdbcddbd432ee5e9a8dfb52f45d41
Joshua Duenow <joshjduenow@gmail.com>
2023-12-02 19:02:05 -0600

    ficing support section

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,46 +7,32 @@ import { HashRouter as Router, Route } from "react-router-dom";
 import FeelingsItem from "../FeelingsItem/FeelingsItem";
 import UnderstandingItem from "../UnderstandingItem/UnderstandingItem";
 
+
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    console.log("in useEffect");
-    getFeelings();
-  }, []);
-  const getFeelings = () => {
-    axios({
-      method: "GET",
-      url: "/feedback",
-    })
-      .then((response) => {
-        const feelingsItem = response.data;
-
-        dispatch({
-          type: "SET_FEEDBACK",
-          payload: feelingsItem,
-        });
-      })
-      .catch((error) => {
-        console.log("error on GET", error);
-      });
-  };
   return (
     <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Feedback!</h1>
+        <h4>Don't forget it!</h4>
+      </header>
       <Router>
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4>Don't forget it!</h4>
-        </header>
         <Route exact path="/">
           <FeelingsItem />
-
-          <Link to="/understandInfo">
-            <button>Next</button>
-          </Link>
         </Route>
-        <Route exact path="/understandInfo">
+        <Route exact path="/understand">
           <UnderstandingItem />
+        </Route>
+        <Route exact path="/support">
+          <SupportItem />
+        </Route>
+        <Route exact path="/comment">
+          <CommentItem />
+        </Route>
+        <Route exact path="/review">
+          <ReviewItem />
+        </Route>
+        <Route exact path="/finish">
+          <FinishItem />
         </Route>
       </Router>
     </div>
---
+++ src/components/SupportItem/SupportItem.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+function SupportItem() {
+  let [supportRate, setSupportRate] = useState("");
+
+  const history = useHistory();
+
+  const dispatch = useDispatch();
+
+  const HandleSupport = (e) => {
+    e.preventDefault();
+
+    dispatch({
+      type: "SET_SUPPORT",
+      payload: supportRate,
+    });
+    history.push("./comment");
+  };
+
+  return (
+    <div>
+      <h1>How well are you being supported ?</h1>
+      <form>
+        <input
+          data-testid="input"
+          placeholder={"1-5"}
+          value={supportRate}
+          type={"number"}
+          onChange={(event) => setSupportRate(event.target.value)}
+        />
+        <button data-testid="next" onClick={HandleSupport}>
+          Next
+        </button>
+      </form>
+    </div>
+  );
+}
+export default SupportItem;
