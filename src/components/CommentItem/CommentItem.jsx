d3f515b223fd4b43794a0b55438be97827ee2dd9
Joshua Duenow <joshjduenow@gmail.com>
2023-12-02 18:37:26 -0600

    working comment section

---
+++ src/components/CommentItem/CommentItem.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+function CommentItem () {
+  let [comment, setComment] = useState("");
+
+  const history = useHistory();
+
+  const dispatch = useDispatch();
+
+  const HandleComment = (e) => {
+    e.preventDefault();
+
+    dispatch({
+      type: "HANDLE_COMMENT",
+      payload: comment,
+    });
+    history.push("./review");
+  };
+
+  return (
+    <div>
+      <h1>Any comments you wanna leave ?</h1>
+      <form>
+        <input
+          data-testid="input"
+          placeholder={"text"}
+          value={comment}
+          type={"text"}
+          onChange={(event) => setComment(event.target.value)}
+        />
+        <button 
+          data-testid="next" 
+          onClick={HandleComment}>
+          Next
+        </button>
+      </form>
+    </div>
+  );
+}
+export default CommentItem;
